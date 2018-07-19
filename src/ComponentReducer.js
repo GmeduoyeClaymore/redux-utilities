addfc2ddf1c92cd654378decaebf1ef347874a03
Gbemiga Meduoye <gbemiga@claymoreminds.com>
2018-07-19 17:37:00 +0100

    Added index

--- src/ComponentReducer.js
+++ src/ComponentReducer.js
@@ -1,12 +1,5 @@
 import {combineReducers} from 'redux-seamless-immutable';
-import {UPDATE_STATE, UPDATE_COMMAND_STATUS, UPDATE_OPTIONS, RESET_ALL_COMPONENT_STATE, UPDATE_COMPONENT_STATE, CLEAR_COMPONENT_STATE} from './ComponentActionConstants';
-
-const dao = (state = {}, action) => {
-  if (action.type && (action.type.startsWith(UPDATE_STATE('')) || action.type.startsWith(UPDATE_OPTIONS('')) || action.type.startsWith(UPDATE_COMMAND_STATUS('')))){
-    return state.setIn(action.path || [], action.data);
-  }
-  return state;
-};
+import {RESET_ALL_COMPONENT_STATE, UPDATE_COMPONENT_STATE, CLEAR_COMPONENT_STATE} from './ComponentActionConstants';
 
 const component = (state = {}, action) => {
   if (action.type && (action.type.startsWith(UPDATE_COMPONENT_STATE('')))){
